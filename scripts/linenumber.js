28b2d7b94017a5aece7025dab529ae9b7bd3e4bc
shadlyd15 <shadlyd15@gmail.com>
2019-06-21 23:37:53 +0600

    Documentation Added

--- scripts/linenumber.js
+++ scripts/linenumber.js
@@ -1,24 +1,30 @@
-'use strict';
+/*global document */
+(function() {
+    var source = document.getElementsByClassName('prettyprint source linenums');
+    var i = 0;
+    var lineNumber = 0;
+    var lineId;
+    var lines;
+    var totalLines;
+    var anchorHash;
+    var lineNumberHTML = '';
 
-/* global document */
-(function () {
-  var lineId, lines, totalLines, anchorHash;
-  var source = document.getElementsByClassName('prettyprint source linenums');
-  var i = 0;
-  var lineNumber = 0;
+    if (source && source[0]) {
+        anchorHash = document.location.hash.substring(1);
+        lines = source[0].getElementsByTagName('li');
+        totalLines = lines.length;
 
-  if (source && source[0]) {
-    anchorHash = document.location.hash.substring(1);
-    lines = source[0].getElementsByTagName('li');
-    totalLines = lines.length;
+        for (; i < totalLines; i++) {
+            lineNumber++;
+            lineId = 'line' + lineNumber;
+            lines[i].id = lineId;
 
-    for (; i < totalLines; i++) {
-      lineNumber++;
-      lineId = 'line' + lineNumber;
-      lines[i].id = lineId;
-      if (lineId === anchorHash) {
-        lines[i].className += ' selected';
-      }
+            lineNumberHTML = '<span class="number">' + (i + 1) + ' : </span>';
+
+            lines[i].insertAdjacentHTML('afterBegin', lineNumberHTML);
+            if (lineId === anchorHash) {
+                lines[i].className += ' selected';
+            }
+        }
     }
-  }
 })();
